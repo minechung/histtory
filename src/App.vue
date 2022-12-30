196ac89b3aecbbf3fa597fb894aac45d6f8efe1d
zz <minechung@163.com>
2022-12-30 16:09:53 +0800

    完善剩余内容

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
   <div class="head_wrapper">
      <div id="head">
-     <van-icon class="topPosition" name="arrow-left" />
+     <van-icon @click="closePage()" class="topPosition" name="arrow-left" />
       History 
     </div>
     <div class="nav_list">
@@ -16,7 +16,7 @@
     <div :class="['all_info',{'height-20px':showArrow}]">
       <div>
         总投注
-        <span>{{betScoreSum}}</span>
+        <span >{{betScoreSum}}</span>
       </div>
       <!-- <div>
         总输赢
@@ -29,14 +29,36 @@
 
   <div id="main_table">
    
-   <div class="table_main">
-      <div class="table_head">注单号：20239943848458483</div>
+   <div v-for="(v,i) in tableData" :key="i" class="table_main">
+      <div class="table_head">Txn.ID &emsp; {{v.OrderID}}</div>
         <div class="main_wrapper" >
-          {{$t('100001')}}
+          <!-- {{$t('100001')}} -->
+          <div class="left_data">
+            <div>{{v.BetTime}}</div>
+            <div>{{columnsData[v.LogType]}}</div>
+            <div>{{$t(v.ServerID)}}</div>
+            <div style="margin-bottom:0">BET：{{v.BetScore}}</div>
+          </div>
+          <div class="right_data">
+            <div>
+              <van-button size="small" color="#7232dd">{{v.StatusName}}</van-button>
+            </div>
+            <div v-if="v.WinScore!==0" class="BetScore">
+              {{v.WinScore}}
+            </div>
+             <div v-if="v.WinScore==0" class="BetScoreLoss">
+              -{{v.BetScore}}
+            </div>
+          </div>
         </div>
    </div>
+  <div v-if="tableData.length==0" class="noinfo">
+    <img src="./assets/no.png" alt="">
+  </div>
+  <div  v-if="tableData.length!==0">
+<van-pagination v-model="currentPage" @change="pageChange" :total-items="total" :items-per-page="10"  mode="simple" />
+  </div>
 
-<van-pagination v-model="currentPage" :page-count="12" mode="simple" />
 
   </div>
   <van-popup v-model="showPicker" round position="bottom">
@@ -56,7 +78,20 @@
     @confirm="onConfirmRight"
   />
 </van-popup>
- <van-calendar  :min-date="minDate" :max-date="maxDate"  type="range" v-model="showDate"  @confirm="changeDate" :value="sureDate" :max-range="7"  />
+ <van-calendar  
+ :min-date="minDate"  
+ :max-date="maxDate"  
+ type="range" 
+ v-model="showDate"  
+ @confirm="changeDate" 
+ :value="sureDate" 
+ :max-range="7"  />
+
+ <van-overlay :show="show" >
+<div class="loading_mask">
+   <van-loading color="#990066" size="30px"  text-size="30px">Loading...</van-loading>
+</div>
+</van-overlay>
   </div>
 </template>
 
@@ -74,7 +109,7 @@ export default {
         "FH",
         "TABLE",
         "LIVE",
-        "ESPORTS",
+        "SPORTS",
         "OTHER",
         "100-PEOPLE",
         "SOLT"
@@ -84,49 +119,80 @@ export default {
       selectRight: "Today",
       sureDate: [],
       showDate: false,
-      minDate: new Date(1900, 1, 1, 0, 0, 0),
-      maxDate: new Date(2099, 12, 31, 59, 59, 59),
+      minDate: new Date(),
+      maxDate: new Date(),
       betScoreSum: 0,
       dataColumns: {
         "ALL-GAME": "",
         FH: "0",
         TABLE: "4",
         LIVE: "10",
-        ESPORTS: "11",
+        SPORTS: "11",
         OTHER: "2",
         "100-PEOPLE": "3",
         SOLT: "1"
       },
-      currentPage: 1
+      columnsData: {
+        "0": "FH",
+        "4": "TABLE",
+        "10": "LIVE",
+        "11": "SPORTS",
+        "2": "OTHER",
+        "3": "100-PEOPLE",
+        "1": "SOLT"
+      },
+      currentPage: 1,
+      tableData: [],
+      show: false,
+      total: 0,
+      userId: ""
+      // servePlatform:''
     };
   },
   mounted() {
+    this.userId = location.search.split("=")[1];
     const end = new Date();
     const start = new Date();
-    start.setTime(start.getTime() - 3600 * 1000 * 24);
+    end.setTime(start.getTime() + 3600 * 1000 * 24);
+    this.minDate = new Date(
+      moment(start.getTime() - 3600 * 1000 * 24 * 6).format("YYYY-MM-DD")
+    );
+    this.maxDate = new Date(
+      moment(start.getTime() + 3600 * 1000 * 24 * 6).format("YYYY-MM-DD")
+    );
     this.sureDate.push(moment(start).format("YYYY-MM-DD"));
+
     this.sureDate.push(moment(end).format("YYYY-MM-DD"));
-    this.getData();
+    this.getData(1);
   },
   methods: {
-    getData() {
+    getData(e) {
+      this.show = true;
+      this.tableData = [];
       getList({
-        page: 1,
-        per_page: 20,
-        user_id: 463499,
+        page: e,
+        per_page: 10,
+        user_id: this.userId,
         gt: this.dataColumns[this.selectLeft],
         date_start: this.sureDate[0],
         date_end: this.sureDate[1]
       })
         .then(res => {
           if (res.data.code == 200) {
-            console.log(res.data);
+            for (let v of res.data.data.data) {
+              v.BetTime = moment(v.BetTime).format("YYYY-MM-DD HH:mm:ss");
+            }
             this.betScoreSum = res.data.data.betScoreSum;
+            this.total = res.data.data.total;
+            this.tableData = res.data.data.data;
+            this.show = false;
           } else {
+            this.show = false;
             this.$toast.fail(res.data.msg);
           }
         })
         .catch(err => {
+          this.show = false;
           console.log(err);
         });
     },
@@ -135,7 +201,7 @@ export default {
     },
     onConfirm(value) {
       this.selectLeft = value;
-      this.getData();
+      this.getData(1);
       this.showPicker = false;
     },
     openPopLeft() {
@@ -147,11 +213,51 @@ export default {
     onConfirmRight(value) {
       this.selectRight = value;
       this.showPickerRight = false;
+      if (value !== "Period") {
+        if (value == "Yester day") {
+          this.sureDate = [];
+          const end = new Date();
+          const start = new Date();
+          start.setTime(start.getTime() - 3600 * 1000 * 24 * 2);
+          end.setTime(end.getTime() - 3600 * 1000 * 24);
+          this.sureDate.push(moment(start).format("YYYY-MM-DD"));
+          this.sureDate.push(moment(end).format("YYYY-MM-DD"));
+          this.currentPage = 1;
+          this.getData(1);
+        } else if (value == "Past 7 day") {
+          this.sureDate = [];
+          const end = new Date();
+          const start = new Date();
+          start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+          this.sureDate.push(moment(start).format("YYYY-MM-DD"));
+          this.sureDate.push(moment(end).format("YYYY-MM-DD"));
+          this.currentPage = 1;
+          this.getData(1);
+        } else if (value == "Today") {
+          this.sureDate = [];
+          const end = new Date();
+          const start = new Date();
+          end.setTime(start.getTime() + 3600 * 1000 * 24);
+          this.sureDate.push(moment(start).format("YYYY-MM-DD"));
+          this.currentPage = 1;
+          this.sureDate.push(moment(end).format("YYYY-MM-DD"));
+          this.getData(1);
+        }
+      }
     },
     changeDate(value) {
       this.sureDate[0] = moment(value[0]).format("YYYY-MM-DD");
       this.sureDate[1] = moment(value[1]).format("YYYY-MM-DD");
       this.showDate = false;
+      this.currentPage = 1;
+      this.getData(1);
+    },
+    pageChange(e) {
+      this.currentPage = e;
+      this.getData(e);
+    },
+    closePage() {
+      location.href = "uniwebview://close";
     }
   }
 };
@@ -242,6 +348,11 @@ export default {
   background: #36095c;
   text-align: center;
   color: #fff;
+  font-weight: 600;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  padding: 0 20px;
 }
 .table_main {
   border: 1px solid #36095c;
@@ -249,6 +360,25 @@ export default {
 }
 .main_wrapper {
   padding: 20px;
+  display: flex;
+  justify-content: space-between;
+}
+.left_data > div {
+  font-size: 14px;
+  color: #fff;
+  font-weight: 400;
+  margin-bottom: 10px;
+}
+.right_data {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: space-between;
+}
+.BetScore {
+  font-size: 20px;
+  font-weight: 600;
+  color: #07c160;
 }
 .van-pagination__item--disabled,
 .van-pagination__item--disabled:active {
@@ -259,5 +389,31 @@ export default {
 }
 .van-pagination__page-desc {
   color: #fff !important;
+}
+.van-pagination__item {
+  color: #fff !important;
+  background: transparent !important;
+}
+.noinfo {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-top: 20px;
+}
+.noinfo > img {
+  width: 100%;
+  margin-bottom: 40px;
+}
+.loading_mask {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.BetScoreLoss {
+  font-size: 20px;
+  font-weight: 600;
+  color: #ff0033;
 }
 </style>
